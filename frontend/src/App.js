cb796aeaa94d0f658d54156aab2c98271a7bff9e
Guy Liba <guy33liba@gmail.com>
2024-05-12 11:50:07 +0200

    fdasf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import NavBar from "./components/NavBar"
 import Home from "./components/Home"
 import Cart from "./components/Cart"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import NewUser from "./components/NewUser"
 import Contexts from "./components/Contexts"
+import NewUser from "./components/new-user/NewUser"
 const App = () => {
   return (
     <Contexts>
---
+++ frontend/src/components/new-user/NewUser.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from "react"
+import axios from "axios"
+import { allStatesContexts } from "../Contexts"
+import "./NewUser.css"
+const NewUser = () => {
+  const { newUser, handleStateChange, users } = useContext(allStatesContexts)
+  const createUser = async () => {
+    try {
+      const { data } = await axios.post("/users", {
+        newUser,
+      })
+      handleStateChange(null, null, data)
+    } catch (error) {
+      console.error("Error creating user:", error)
+    }
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    createUser()
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="inputs">
+        <div>
+          <div>
+            <input
+              type="text"
+              value={newUser.name}
+              onChange={(e) => handleStateChange("name", e.target.value)}
+              placeholder="Name"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              value={newUser.email}
+              onChange={(e) => handleStateChange("email", e.target.value)}
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={newUser.password}
+              onChange={(e) => handleStateChange("password", e.target.value)}
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className="checkbox">
+          <div>Is Admin</div>
+          <input
+            type="checkbox"
+            checked={newUser.isAdmin}
+            onChange={(e) => handleStateChange("isAdmin", e.target.checked)}
+          />
+
+          <button type="submit">Create User</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default NewUser
