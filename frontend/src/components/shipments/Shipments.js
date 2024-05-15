c27cf99bbc95c0864916ce0cf6e4b2b6365b96ec
Guy Liba <guy33liba@gmail.com>
2024-05-15 14:54:50 +0200

    dasd

--- frontend/src/components/shipments/Shipments.js
+++ frontend/src/components/shipments/Shipments.js
@@ -8,7 +8,7 @@ const Shipments = () => {
     const fetchShipments = async () => {
       try {
         const { data } = await axios.get("/shipments")
-        setShipments(data)
+        setShipments([...data])
         console.log(data)
       } catch (error) {
         console.error("Error fetching shipments:", error.message)
@@ -23,10 +23,11 @@ const Shipments = () => {
       <h1>Shipments</h1>
       {Array(shipments).map((shipment, index) => (
         <div key={index}>
-          <p>Address: {shipment.address}</p>
-          <p>City: {shipment.city}</p>
-          <p>Postal Code: {shipment.postalCode}</p>
-          <p>Country: {shipment.country}</p>
+          <h4>Address : {shipment.address}</h4>
+          <h4>City : {shipment.city}</h4>
+          <h4>Postal Code : {shipment.postalCode}</h4>
+          <h4>Country : {shipment.country}</h4>
+          <h2>Payment Method : {shipment.paymentMethod}</h2>
         </div>
       ))}
     </div>
