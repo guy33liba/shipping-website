2f2693ab2e2d4f3db2b3eef20003c182bf45eeff
guy33liba <guy33liba@gmail.com>
2024-05-15 21:48:42 +0300

    hgv

--- frontend/src/components/users-list/UsersList.js
+++ frontend/src/components/users-list/UsersList.js
@@ -18,10 +18,10 @@ const UsersList = () => {
         return (
           <div key={index}>
             <div style={{}}>
-              <h2>User Name:: {user.name}</h2>
-              <h2>email:: {user.email}</h2>
-              <h2>password: {user.password}</h2>
-              <h2>isAdmin: {user.isAdmin.toString()}</h2>
+              <h2>User Name : {user.name}</h2>
+              <h2>email : {user.email}</h2>
+              <h2>password : {user.password}</h2>
+              <h2>isAdmin : {user.isAdmin.toString()}</h2>
             </div>
           </div>
         )
