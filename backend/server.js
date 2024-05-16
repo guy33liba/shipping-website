cc16eaeb563174aeb0eb2c7802e40ed0907c1f9e
Guy Liba <guy33liba@gmail.com>
2024-05-16 13:59:45 +0200

    dasf

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ import productRouter from "./routes/productsRoute.js"
 //
 export const mongoURI =
   "mongodb+srv://guy33liba:g33g33g33@shipping.bnmim8g.mongodb.net/?retryWrites=true&w=majority&appName=shipping"
+
 mongoose.connect(mongoURI)
-//
 const app = express()
+
 app.use(express.json())
-//
 app.use(cors())
+//
 app.use("/users", userRouter)
 app.use("/shipments", shippingRouter)
 app.use("/products", productRouter)
