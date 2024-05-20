7f32628644fabb3c908412f7130cbb9d5b8a920e
Guy Liba <guy33liba@gmail.com>
2024-05-20 13:44:51 +0200

    fasdf

--- user-auth-backend/server.js
+++ user-auth-backend/server.js
@@ -7,11 +7,11 @@ const app = express()
 app.use(express.json())
 const posts = [
   { username: "Kyle", title: "Post 1" },
-  { username: "Jim", title: "Post 1" },
+  { username: "Jim", title: "Post 2" },
 ]
 
 app.get("/posts", authenticateToken, (req, res) => {
-  res.send(posts.filter((post => post.username === req.user.name)))
+  res.send(posts.filter((post) => post.username === req.user.name))
 })
 app.post("/login", (req, res) => {
   const username = req.body.username
