e3fcf96317577b5ed61cd5c09c4e2c9d6fd80686
Guy Liba <guy33liba@gmail.com>
2024-05-21 17:03:25 +0200

    fdas

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,8 +1,12 @@
-import express from "express"
-import User from "../model/userSchema.js"
+import express from 'express'
+import User from '../model/userSchema.js'
+import { protect } from '../middleware/authMiddleware.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const userRouter = express.Router()
-userRouter.get("/", async (req, res) => {
+
+userRouter.get('/', protect, async (req, res) => {
   try {
     const users = await User.find({})
     res.json(users)
@@ -11,7 +15,7 @@ userRouter.get("/", async (req, res) => {
   }
 })
 
-userRouter.post("/", async (req, res) => {
+userRouter.post('/', async (req, res) => {
   const { name, email, password, isAdmin } = req.body.register
   const user = new User({
     register: {
@@ -29,17 +33,33 @@ userRouter.post("/", async (req, res) => {
   }
 })
 
-userRouter.get("/:userId", (req, res) => {
+userRouter.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ 'register.email': email })
+    if (user && (await user.matchPassword(password))) {
+      const token = user.getSignedJwtToken()
+      res.json({ token, user })
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+userRouter.get('/:userId', protect, (req, res) => {
   const userId = req.params.userId
   res.json({ message: `Get user with ID ${userId}` })
 })
 
-userRouter.put("/:userId", (req, res) => {
+userRouter.put('/:userId', protect, (req, res) => {
   const userId = req.params.userId
   res.json({ message: `Update user with ID ${userId}` })
 })
 
-userRouter.delete("/:userId", (req, res) => {
+userRouter.delete('/:userId', protect, (req, res) => {
   const userId = req.params.userId
   res.json({ message: `Delete user with ID ${userId}` })
 })
