ec9614c4af3aa878ef786959da2e482f5e571302
Guy Liba <guy33liba@gmail.com>
2024-06-03 11:52:51 +0200

    fas

--- itemsbackend/server.js
+++ itemsbackend/server.js
@@ -10,7 +10,10 @@ app.use(express.json())
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/yourDatabaseName", {})
+  .connect(
+    "mongodb+srv://guy33liba:g33g33g33@shipping.bnmim8g.mongodb.net/?retryWrites=true&w=majority&appName=shipping",
+    {},
+  )
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err))
 
