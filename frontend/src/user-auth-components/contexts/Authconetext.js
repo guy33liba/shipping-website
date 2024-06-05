41eb43a1bca1db449372fae6062c52eb103e0774
Guy Liba <guy33liba@gmail.com>
2024-06-05 16:29:56 +0200

    fasd

--- frontend/src/user-auth-components/contexts/Authconetext.js
+++ frontend/src/user-auth-components/contexts/Authconetext.js
@@ -1,54 +1,59 @@
-import { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
+import { createContext, useState, useEffect } from "react"
+import axios from "axios"
 
-const AuthContext = createContext();
+const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
   const [authState, setAuthState] = useState({
-    token: localStorage.getItem('token'),
+    token: localStorage.getItem("token"),
     isAuthenticated: null,
     user: null,
-  });
+  })
 
   useEffect(() => {
     const loadUser = async () => {
-      if (localStorage.getItem('token')) {
-        axios.defaults.headers.common['x-auth-token'] = localStorage.getItem('token');
+      if (localStorage.getItem("token")) {
+        axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token")
       }
 
       try {
-        const res = await axios.get('/api/auth/user');
-        setAuthState({ token: localStorage.getItem('token'), isAuthenticated: true, user: res.data });
+        const res = await axios.get("/api/auth/user")
+        setAuthState({
+          token: localStorage.getItem("token"),
+          isAuthenticated: true,
+          user: res.data,
+        })
       } catch (err) {
-        setAuthState({ token: null, isAuthenticated: false, user: null });
+        setAuthState({ token: null, isAuthenticated: false, user: null })
       }
-    };
+    }
 
-    loadUser();
-  }, []);
+    loadUser()
+  }, [])
 
   const register = async (formData) => {
-    const res = await axios.post('/api/auth/register', formData);
-    localStorage.setItem('token', res.data.token);
-    setAuthState({ token: res.data.token, isAuthenticated: true, user: null });
-  };
+    const res = await axios.post("/api/auth/register", formData)
+    localStorage.setItem("token", res.data.token)
+    setAuthState({ token: res.data.token, isAuthenticated: true, user: null })
+  }
 
   const login = async (formData) => {
-    const res = await axios.post('/api/auth/login', formData);
-    localStorage.setItem('token', res.data.token);
-    setAuthState({ token: res.data.token, isAuthenticated: true, user: null });
-  };
+    const res = await axios.post("/api/auth/login", formData)
+    localStorage.setItem("token", res.data.token)
+    setAuthState({ token: res.data.token, isAuthenticated: true, user: null })
+  }
 
   const logout = () => {
-    localStorage.removeItem('token');
-    setAuthState({ token: null, isAuthenticated: false, user: null });
-  };
+    localStorage.removeItem("token")
+    setAuthState({ token: null, isAuthenticated: false, user: null })
+  }
 
   return (
     <AuthContext.Provider value={{ authState, register, login, logout }}>
       {children}
     </AuthContext.Provider>
-  );
-};
+  )
+}
 
-export { AuthProvider, AuthContext };
+export { AuthProvider, AuthContext }
+
