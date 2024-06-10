db22faedcbb73d55230805bd25b125a1cda29462
guy33liba <guy33liba@gmail.com>
2024-06-10 22:20:59 +0300

    fas

--- auth-backend/models/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const router = express.Router()
-import { register, login, getUser } from "../controllers/authController.js"
-import authMiddleware from "./authMiddlerWare.js"
-router.post("/register", register)
-router.post("/login", login)
-router.get("/user", authMiddleware, getUser)
-
-export default router
--- exercise.js
+++ exercise.js
@@ -16,7 +16,7 @@ function reverseString(str) {
   return str.split("").reverse().join("")
 }
 console.log(reverseString("hello"))
-console.log(palindrome("nitin"))
+console.log(palindrome(str3))
 function factorialize(num) {
   if (num < 0) return -1
   else if (num == 0) return 1
@@ -25,20 +25,6 @@ function factorialize(num) {
   }
 }
 console.log(factorialize(12))
-
-function findLongestWord(str) {
-  let largest = ""
-  console.log(str.split(" "))
-  str = str.split(" ")
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].length > largest.length) {
-      largest = str[i]
-    }
-  }
-  return largest
-}
-
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog")) // 6
 
 function palindrome(str) {
   let len = str.length
@@ -208,3 +194,51 @@ function removeDuplicates(arr) {
 // Example usage:
 // console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])) // Should return [1, 2, 3, 4, 5]
 // console.log(removeDuplicates([10, 10, 20, 20, 30])) // Should return [10, 20, 30]
+
+function getwordlength(str) {
+  const words = str.split(" ")
+  let wordslength = words.map((word) => word.length)
+  return console.log(wordslength)
+}
+
+function palindrome1(str) {
+  let j = str.length - 1
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[j]) {
+      return false
+    }
+    j--
+  }
+  return true
+}
+// console.log(palindrome1("heloo"))
+
+let sentence = "words are like this is asnot fdasdgood acquisition blabla"
+
+function biggestwrwrw(str) {
+  let words = str.split(" ")
+  console.log(words)
+  let biggestWord = ""
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > biggestWord.length) {
+      biggestWord = words[i];
+    }
+  }
+  return biggestWord
+}
+
+
+console.log(biggestwrwrw(sentence))
+function findLongestWord(str) {
+  let largest = ""
+  console.log(str.split(" "))
+  str = str.split(" ")
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].length > largest.length) {
+      largest = str[i]
+    }
+  }
+  return largest
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")) // 6
