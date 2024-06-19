b25c54a817564eea85d90be4465a611a608628ba
guy33liba <guy33liba@gmail.com>
2024-06-19 21:57:00 +0300

    fdsa

--- exercise.js
+++ exercise.js
@@ -804,5 +804,13 @@ const findNumberClosestTo100 = (a, b) => {
 console.log(findNumberClosestTo100(78, 88))
 
 const countChars = (str, char) => {
-  str.split("").filter((ch) => ch === char)
+  return str.split("").filter((ch) => ch === char).length
 }
+const contains2To4 = (str, char) => {
+  return countChars(str, char) >= 2 && countChars(str, char) <= 4
+}
+console.log(contains2To4("oh!", "o"))
+console.log(contains2To4("ooh!", "o"))
+console.log(contains2To4("ooooooh!", "o"))
+console.log(contains2To4("ooh!", "o"))
+console.log(contains2To4("ooh!", "o"))
